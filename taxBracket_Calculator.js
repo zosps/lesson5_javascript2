858d27eff436c0f98195c89ac9150a119e077b62
Zoe Patrick <zp15c@my.fsu.edu>
2018-03-01 20:34:31 +0000

    Lesson 5 final commit

--- taxBracket_Calculator.js
+++ taxBracket_Calculator.js
@@ -76,13 +76,44 @@ function calculateTax(){
            
            
            
-          else if(status == 1) {
+           if(status == 1) {
                 //INSERT YOUR CODE BELOW
   
-  
-  
-  
-  
+                if (income <= 19050){
+                 tax = income * 0.10;
+                 taxRate = 10;
+               }//end of nested if
+               else if (income <= 77400) {
+                 tax = 19050 * 0.10 + (income - 19050) * 0.12;
+                 taxRate = 12;
+               }//end of else if
+               else if (income <= 165000) {
+                 tax = 19050 * 0.10 + (77400 - 19050) * 0.12 + (income - 77400) * 0.22;
+                 taxRate = 22;
+               } //end of else if
+               else if (income <= 315000) {
+                 tax = 19050 * 0.10 + (77400 - 19050) * 0.12 +
+                       (165000 - 77400) * 0.22 + (income - 165000) * 0.24;
+                 taxRate = 24;
+               }//end of else if
+               else if (income <= 400000) {
+                 tax = 19050 * 0.10 + (77400 - 19050) * 0.12 +
+                       (165000 - 77400) * 0.22 + (315000 - 165000) * 0.24 +
+                       (income - 315000) * 0.32;
+                 taxRate = 32;
+               }//end of else if
+               else if (income <= 600000) {
+                  tax = 19050 * 0.10 + (77400 - 19050) * 0.12 +
+                        (165000 - 77400) * 0.22 + (315000 - 165000) * 0.24 +
+                        (400000 - 315000) * 0.32 + (income - 400000) * 0.35;
+                  taxRate = 35;
+               }//end of else if
+               else {
+                  tax = 19050 * 0.10 + (77400 - 19050) * 0.12 +
+                        (165000 - 77400) * 0.22 + (315000 - 165000) * 0.24 +
+                        (400000 - 315000) * 0.32 + (600000 - 400000 ) * 0.35 + (income - 600000) * .37;
+                  taxRate = 37;
+               }//end of nested if
   
           }//end of married file jointly calculation
           
@@ -103,11 +134,44 @@ function calculateTax(){
                  
                  
                  
-          else if(status == 2) {
+           if(status == 2) {
                 //INSERT YOUR CODE BELOW
-
+                if (income <= 9525){
+                 tax = income * 0.10;
+                 taxRate = 10;
+               }//end of nested if
+               else if (income <= 38700) {
+                 tax = 9525 * 0.10 + (income - 9525) * 0.12;
+                 taxRate = 12;
+               }//end of else if
+               else if (income <= 82500) {
+                 tax = 9525 * 0.10 + (38700 - 9525) * 0.12 + (income - 38700) * 0.22;
+                 taxRate = 22;
+               } //end of else if
+               else if (income <= 157500) {
+                 tax = 9525 * 0.10 + (38700 - 9525) * 0.12 +
+                       (82500 - 38700) * 0.22 + (income - 82500) * 0.24;
+                 taxRate = 24;
+               }//end of else if
+               else if (income <= 200000) {
+                 tax = 9525 * 0.10 + (38700 - 9525) * 0.12 +
+                       (82500 - 38700) * 0.22 + (157500 - 82500) * 0.24 +
+                       (income - 157500) * 0.32;
+                 taxRate = 32;
+               }//end of else if
+               else if (income <= 300000) {
+                  tax = 9525 * 0.10 + (38700 - 9525) * 0.12 +
+                        (82500 - 38700) * 0.22 + (157500 - 82500) * 0.24 +
+                        (200000 - 157500) * 0.32 + (income - 200000) * 0.35;
+                  taxRate = 35;
+               }//end of else if
+               else {
+                  tax = 9525 * 0.10 + (38700 - 9525) * 0.12 +
+                        (82500 - 38700) * 0.22 + (157500 - 82500) * 0.24 +
+                        (200000 - 157500) * 0.32 + (300000 - 200000 ) * 0.35 + (income - 300000) * .37;
+                  taxRate = 37;
              
-             
+             }//end of nested if
              
           }//end of married file separately calculation
         
@@ -128,12 +192,45 @@ function calculateTax(){
           
           
           
-          else if(status == 3) {
+           if(status == 3) {
                 //INSERT YOUR CODE BELOW
- 
+                if (income <= 13600){
+                 tax = income * 0.10;
+                 taxRate = 10;
+               }//end of nested if
+               else if (income <= 51800) {
+                 tax = 13600 * 0.10 + (income - 13600) * 0.12;
+                 taxRate = 12;
+               }//end of else if
+               else if (income <= 82500) {
+                 tax = 13600 * 0.10 + (51800 - 13600) * 0.12 + (income - 51800) * 0.22;
+                 taxRate = 22;
+               } //end of else if
+               else if (income <= 157500) {
+                 tax = 13600 * 0.10 + (51800 - 13600) * 0.12 +
+                       (82500 - 51800) * 0.22 + (income - 82500) * 0.24;
+                 taxRate = 24;
+               }//end of else if
+               else if (income <= 200000) {
+                 tax = 13600 * 0.10 + (51800 - 13600) * 0.12 +
+                       (82500 - 51800) * 0.22 + (157500 - 82500) * 0.24 +
+                       (income - 157500) * 0.32;
+                 taxRate = 32;
+               }//end of else if
+               else if (income <= 500000) {
+                  tax = 13600 * 0.10 + (51800 - 13600) * 0.12 +
+                        (82500 - 51800) * 0.22 + (157500 - 82500) * 0.24 +
+                        (200000 - 157500) * 0.32 + (income - 200000) * 0.35;
+                  taxRate = 35;
+               }//end of else if
+               else {
+                  tax = 13600 * 0.10 + (51800 - 13600) * 0.12 +
+                        (82500 - 51800) * 0.22 + (157500 - 82500) * 0.24 +
+                        (200000 - 157500) * 0.32 + (500000 - 200000 ) * 0.35 + (income - 500000) * .37;
+                  taxRate = 37;
  
 
-
+                    }//end of nested if
              
           }//end of head of household calculation
           
@@ -201,10 +298,14 @@ function clearButton () {
               document.getElementById("filingStatus").selectedIndex = 0;
               
              //INSERT YOUR CODE BELOW
+             
+             
+            
+             //This code clears the estimated tax
+               document.getElementById("taxEstimate").innerHTML = "";
+              
+             //This code clears the tax bracket
+               document.getElementById("taxBracket").innerHTML = "";
 
-
-
-
-
-}//end of clearButton function
+        }//end of clearButton function
       
